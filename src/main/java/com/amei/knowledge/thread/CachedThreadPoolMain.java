40f93206a8f3cb0e46960ca361ece8895cc8dbe4
limeihua-dev <1344489235@qq.com>
2022-04-21 11:28:41 +0800

    可缓存的线程池

--- src/main/java/com/amei/knowledge/thread/CachedThreadPoolMain.java
+++ src/main/java/com/amei/knowledge/thread/CachedThreadPoolMain.java
@@ -1,4 +1,25 @@
 package com.amei.knowledge.thread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class CachedThreadPoolMain {
+    public static void main(String[] args) {
+        // 创建任务
+        Task task1 = new Task();
+        Task task2 = new Task();
+        Task task3 = new Task();
+        // 创建可缓存的线程池
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        //提交任务
+        threadPool.execute(task1);
+        threadPool.execute(task2);
+        threadPool.execute(task3);
+        //关闭线程池
+        threadPool.shutdown();
+        //打印
+//        pool-1-thread-1
+//        pool-1-thread-3
+//        pool-1-thread-2
+    }
 }
