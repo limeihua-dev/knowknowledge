b5be869388136d5dfd88f356bfcc1fd0751b0a96
limeihua-dev <1344489235@qq.com>
2022-06-01 09:36:05 +0800

    yaml修改

--- src/main/java/com/amei/knowledge/rabbitMq/RabbitDemoConsumer.java
+++ src/main/java/com/amei/knowledge/rabbitMq/RabbitDemoConsumer.java
@@ -1,6 +1,7 @@
 package com.amei.knowledge.rabbitMq;
 
 import com.amei.knowledge.rabbitMq.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,9 @@ import java.util.Map;
 @RabbitListener(queues = {RabbitMQConfig.RABBITMQ_DEMO_TOPIC})
 public class RabbitDemoConsumer {
 
+    @RabbitHandler
     public void process(Map map) {
         System.out.println("消费者RabbitDemoConsumer从RabbitMQ服务端消费消息：" + map.toString());
     }
+
 }
