0449401c92b3296b5d5d46452d0453af52b537b4
limeihua-dev <1344489235@qq.com>
2022-06-13 12:09:15 +0800

    原型设计模式

--- src/main/java/com/amei/knowledge/设计模式/原型模式/prototypeMain.java
+++ src/main/java/com/amei/knowledge/设计模式/原型模式/prototypeMain.java
@@ -2,7 +2,7 @@ package com.amei.knowledge.设计模式.原型模式;
 
 import com.amei.knowledge.设计模式.原型模式.impl.ProductImpl;
 
-public class main {
+public class prototypeMain {
     public static void main(String[] args) throws CloneNotSupportedException {
         //准备
         Manager manager = new Manager();
